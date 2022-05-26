3e1ede857898fade16f2a9686e2daadf0b6a3805
Alumno <alumno@cepibase.int>
2022-05-26 11:30:10 +0200

    agregao una nueva funcion

--- principal.py
+++ principal.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, redirect
 from bson import ObjectId
 from pymongo import MongoClient
 
+def comprueba_fondo():
+	pass
+
+DOCUMENTOS = ['doc', 'docx']
+
 app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017')
