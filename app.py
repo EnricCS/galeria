fe151203c614b14fdf3d5612601c4a5ffc1ce1ee
Alumno <alumno@cepibase.int>
2022-05-26 09:40:14 +0200

    mi primer commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from bson import ObjectId
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017')
+basedatos = client.fondos
+misfondos = basedatos.lista
+
+@app.route('/')
+@app.route('/aportar', methods = ['POST'])
+def agregar():
+    
+    tit = request.values.get('titulo')
+    desc = request.values.get('descripcion')
+    tags = []
+    lista=['animales','naturaleza', 'ciudad', 'deporte', 'personas']
+    for tema in lista:
+        if request.values.get(tema):
+            tags += tema
+    misfondos.insert({'título': tit, 'descripción': desc, 'tags': tags})
+    return redirect('/insertar')
+
+@app.route('/insertar', methods = ['POST'])
+if __name__ == '__main__':
+    app.run()
